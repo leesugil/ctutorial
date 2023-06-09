b341cfafebc9fbb725b94322440203d570e1410f
sugilmath <sugilmath@gmail.com>
2023-06-09 14:13:32 -0500

    note on string constant, character constant, and escape sequences

--- line_counting.c
+++ line_counting.c
@@ -10,4 +10,14 @@ main()
 		if (c == '\n')
 			++nl;
 	printf("%d\n", nl);
+
+	/* string constant, character constant, and escape sequences */
+	printf("More on string constants and character constants\n"
+		"A thing like \"hello, world!\" is called a character string or a string constant.\n"
+		"A thing like \'a\' is called a character constant.\n"
+		"Note that there are escape sequences such as \'\\n\' for new line,"
+		"\'\\t\' for a tab, or \'\\b\' for backspace when used in double quotes.\n"
+		"These escape sequences in a single quote also can be a character constant.\n"
+		"For example, \'\\n\' is integer 10 as the value representing the newline character.\n"
+		"%d\n", '\n');
 }
