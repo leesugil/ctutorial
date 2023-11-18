0a4ba2c177e25b95d3692c57a89afc627125637e
Sugil Steve Lee <sugilmath@gmail.com>
2023-11-17 19:48:33 -0500

    exercise 4-2:

--- exercise4-2.c
+++ exercise4-2.c
@@ -1,18 +1,21 @@
+#include <stdio.h>
 #include <ctype.h>
+#define MAXLINE	1000
 
 /* atof: convert string s to double */
 double atof(char s[])
 {
 	double val, power, outcome;
 	int i, sign, powersign;
-
+	
 	for (i = 0; isspace(s[i]); i++)	/* skip white space */
 		;
 	sign = (s[i] == '-') ? -1 : 1;
 	if (s[i] == '+' || s[i] == '-')
 		i++;	/* skip sign character */
-	for (val = 0.0; isdigit(s[i]); i++)
+	for (val = 0.0; isdigit(s[i]); i++) {
 		val = 10.0 * val + (s[i] - '0');
+	}
 	if (s[i] = '.')
 		i++;	/* skip decimal point character */
 	for (power = 1.0; isdigit(s[i]); i++) {
@@ -20,12 +23,12 @@ double atof(char s[])
 		power *= 10;
 	}
 	outcome = sign * val / power;
+	/* read the exponential part of scientific notation of the form 123.4e-6 or 123.4E7 */
 	if (s[i] == 'e' || s[i] == 'E')
 		i++;
-	if (s[i] == '-' || s[i] == '+') {
+	powersign = (s[i] == '-') ? -1 : 1;
+	if (s[i] == '-' || s[i] == '+')
 		i++;
-		powersign = (s[i] == '-') ? -1 : 1;
-	}
 	for (val = 0.0; isdigit(s[i]); i++)
 		val = 10.0 * val + (s[i] - '0');
 	for (i = 0; i < val; i++) {
@@ -35,4 +38,14 @@ double atof(char s[])
 			outcome /= 10;
 	}
 	return outcome;
+}
+
+main ()
+{
+	char line[MAXLINE];
+	double atof(char []);
+
+	while(getline(line, MAXLINE) > 0) {
+		printf("%f\n", atof(line));
+	}
 }
