2d5256f4e83eedcc4f2139eda406e83c05c5ffb1
Sugil Steve Lee <sugilmath@gmail.com>
2023-11-17 19:54:21 -0500

    codes from the book have been accumulated and getting reused in later sections, so i'll try to put all the function codes in tutoriallib.c and keep exercise codes simply as made of main() only. when compiling, use 'gcc tutoriallib.c exercisex-y.c -ansi'

--- exercise4-2.c
+++ exercise4-2.c
@@ -1,44 +1,6 @@
 #include <stdio.h>
-#include <ctype.h>
-#define MAXLINE	1000
 
-/* atof: convert string s to double */
-double atof(char s[])
-{
-	double val, power, outcome;
-	int i, sign, powersign;
-	
-	for (i = 0; isspace(s[i]); i++)	/* skip white space */
-		;
-	sign = (s[i] == '-') ? -1 : 1;
-	if (s[i] == '+' || s[i] == '-')
-		i++;	/* skip sign character */
-	for (val = 0.0; isdigit(s[i]); i++) {
-		val = 10.0 * val + (s[i] - '0');
-	}
-	if (s[i] = '.')
-		i++;	/* skip decimal point character */
-	for (power = 1.0; isdigit(s[i]); i++) {
-		val = 10.0 * val + (s[i] - '0');
-		power *= 10;
-	}
-	outcome = sign * val / power;
-	/* read the exponential part of scientific notation of the form 123.4e-6 or 123.4E7 */
-	if (s[i] == 'e' || s[i] == 'E')
-		i++;
-	powersign = (s[i] == '-') ? -1 : 1;
-	if (s[i] == '-' || s[i] == '+')
-		i++;
-	for (val = 0.0; isdigit(s[i]); i++)
-		val = 10.0 * val + (s[i] - '0');
-	for (i = 0; i < val; i++) {
-		if (powersign > 0)
-			outcome *= 10;
-		else
-			outcome /= 10;
-	}
-	return outcome;
-}
+#define MAXLINE	1000
 
 main ()
 {
--- tutoriallib.c
+++ tutoriallib.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 
 /* getline: get line into s, return length */
 int getline(char s[], int lim)
@@ -12,4 +13,42 @@ int getline(char s[], int lim)
 		s[i++] = c;
 	s[i] = '\0';
 	return i;
+}
+
+/* atof: convert string s to double */
+double atof(char s[])
+{
+	double val, power, outcome;
+	int i, sign, powersign;
+	
+	for (i = 0; isspace(s[i]); i++)	/* skip white space */
+		;
+	sign = (s[i] == '-') ? -1 : 1;
+	if (s[i] == '+' || s[i] == '-')
+		i++;	/* skip sign character */
+	for (val = 0.0; isdigit(s[i]); i++) {
+		val = 10.0 * val + (s[i] - '0');
+	}
+	if (s[i] = '.')
+		i++;	/* skip decimal point character */
+	for (power = 1.0; isdigit(s[i]); i++) {
+		val = 10.0 * val + (s[i] - '0');
+		power *= 10;
+	}
+	outcome = sign * val / power;
+	/* read the exponential part of scientific notation of the form 123.4e-6 or 123.4E7 */
+	if (s[i] == 'e' || s[i] == 'E')
+		i++;
+	powersign = (s[i] == '-') ? -1 : 1;
+	if (s[i] == '-' || s[i] == '+')
+		i++;
+	for (val = 0.0; isdigit(s[i]); i++)
+		val = 10.0 * val + (s[i] - '0');
+	for (i = 0; i < val; i++) {
+		if (powersign > 0)
+			outcome *= 10;
+		else
+			outcome /= 10;
+	}
+	return outcome;
 }
