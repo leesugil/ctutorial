787a5fe59a00d7876c6692e5e409572439b3c92c
Sugil Steve Lee <sugilmath@gmail.com>
2023-11-19 04:05:20 -0500

    found a critical error in the solution of 4-3, reworking again

--- exercise4-5.c
+++ exercise4-5.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>	/* for atof() */
+#include <math.h>	/* for math library functions */
 
 #define	MAXOP	100	/* max size of operand or operator */
 #define	NUMBER	'0'	/* signal that a number was found */
@@ -9,7 +10,7 @@ void push(double);
 double pop(void);
 
 /* reverse Polish calculator */
-/* Add the modulus (%) operator and provisions for negative numbers */
+/* Add access to library functions like sin, exp, and pow */
 main()
 {
 	int type;
@@ -39,11 +40,11 @@ main()
 					printf("error: zero divisor\n");
 				break;
 			case '%':
-				op2 = (int) pop();
-				if (op2 != 0)
+				op2 = pop();
+				if (op2 != 0.0)
 					push(((int) pop()) % ((int) op2));
 				else
-					printf("error: zero modulo\n");
+					printf("error: modulo zero!\n");
 				break;
 			case '\n':
 				printf("\t%.8g\n", pop());
@@ -52,6 +53,7 @@ main()
 				printf("error: unknown command %s\n", s);
 				break;
 		}
+		printf("getop done!\n");
 	}
 	return 0;
 }
@@ -81,6 +83,48 @@ double pop(void)
 	}
 }
 
+/* ptop: print the top element without popping */
+double ptop(void)
+{
+	if (sp > 0)
+		return val[sp];
+	else {
+		printf("error: stack empty\n");
+		return 0.0;
+	}
+}
+
+/* dtop: duplicate the top element */
+void dtop(void)
+{
+	if (sp > 0)
+		push(ptop());
+	else
+		printf("error: stack empty\n");
+}
+
+/* swap: swap the top two elements */
+void swap(void)
+{
+	double op1, op2;
+
+	if (sp > 1) {
+		op2 = pop();
+		op1 = pop();
+		push(op2);
+		push(op1);
+	}
+	else
+		printf("error: stack too low to swap\n");
+}
+
+/* clear: clear the stack */
+void clear(void)
+{
+	sp = 0;
+	val[sp] = 0.0;
+}
+
 #include <ctype.h>
 
 int getch(void);
@@ -89,23 +133,34 @@ void ungetch(int);
 /* getop: get next character or numeric operand */
 int getop(char s[])
 {
-	int i, c;
+	int i, c, sign;
 
-	while ((s[0] = c = getch()) == ' ' || c == '\t')
+	while((s[0] = c = getch()) == ' ' || c == '\t')
 		;
 	s[1] = '\0';
-	if (!isdigit(c) && c != '.')
-		return c;	/* not a number */
+	printf("initial s: %s\n", s);
 	i = 0;
+	if (c != '-' && !isdigit(c) && c != '.') {
+		/* collect string operator */
+		while ('A' <= (s[++i] = c = getch()) && c <= 'z')
+			;
+		s[i] = '\0';
+		return c;	/* not a number */
+	}
+	if (c == '-')	/* get ready to collect digits */
+		s[++i] = c = getch();
 	if (isdigit(c))	/* collect integer part */
 		while (isdigit(s[++i] = c = getch()))
-			;
+			printf("integer part: %d\n", c - '0');
 	if (c == '.')	/* collect fraction part */
 		while (isdigit(s[++i] = c = getch()))
-			;
+			printf("fraction part: %d\n", c - '0');
 	s[i] = '\0';
-	if (c != EOF)
+	printf("terminal s: %s\n", s);
+	if (c != EOF) {
 		ungetch(c);
+		printf("c != EOF, c: %d\n", c);
+	}
 	return NUMBER;
 }
 
