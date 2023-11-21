4b182c62cf17f23573f0294ce5c688439959730b
leesugil <sugilmath@gmail.com>
2023-11-20 19:25:20 -0500

    in the middle of solving

--- exercise4-5.c
+++ exercise4-5.c
@@ -1,26 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>	/* for atof() */
-#include <math.h>	/* for math library functions */
+#include <ctype.h>	/* for isdigit() */
+#include <math.h>
+#include <string.h>	/* for strlen() */
 
 #define	MAXOP	100	/* max size of operand or operator */
 #define	NUMBER	'0'	/* signal that a number was found */
+#define MATH	'-1'	/* signal that math library is called */
 
 int getop(char []);
 void push(double);
 double pop(void);
+void clear(void);
+void math(char []);
 
 /* reverse Polish calculator */
-/* Add access to library functions like sin, exp, and pow */
+/* Add access to library functions like sin, exp, and pow. */
 main()
 {
 	int type;
-	double op2;
+	double op2, op3;
 	char s[MAXOP];
+
+	printf("type 'h' for commands\n");
 
 	while ((type = getop(s)) != EOF) {
 		switch (type) {
 			case NUMBER:
 				push(atof(s));
+				break;
+			case MATH:
+				math(s);
 				break;
 			case '+':
 				push(pop() + pop());
@@ -40,11 +50,38 @@ main()
 					printf("error: zero divisor\n");
 				break;
 			case '%':
-				op2 = pop();
-				if (op2 != 0.0)
+				op2 = (int) pop();
+				if (op2 != 0)
 					push(((int) pop()) % ((int) op2));
 				else
-					printf("error: modulo zero!\n");
+					printf("error: zero modulo\n");
+				break;
+			case 'h':
+				/* print commands */
+				printf("p: print top\nd: duplicate top\ns: swap top two\nc: clear stack\n");
+				break;
+			case 'p':
+				/* print top */
+				push(op2 = pop());
+				push(op2); /* to be popped by '\n' for print */
+				break;
+			case 'd':
+				/* duplicate top */
+				push(op2 = pop());
+				push(op2);
+				push(op2); /* to be popped by '\n' for print */
+				break;
+			case 's':
+				/* swap top */
+				op2 = pop();
+				op3 = pop();
+				push(op2);
+				push(op3);
+				push(op3); /* to be popped by '\n' for print */
+				break;
+			case 'c':
+				/* clear stack */
+				clear();
 				break;
 			case '\n':
 				printf("\t%.8g\n", pop());
@@ -53,7 +90,6 @@ main()
 				printf("error: unknown command %s\n", s);
 				break;
 		}
-		printf("getop done!\n");
 	}
 	return 0;
 }
@@ -83,49 +119,34 @@ double pop(void)
 	}
 }
 
-/* ptop: print the top element without popping */
-double ptop(void)
-{
-	if (sp > 0)
-		return val[sp];
-	else {
-		printf("error: stack empty\n");
-		return 0.0;
-	}
-}
-
-/* dtop: duplicate the top element */
-void dtop(void)
-{
-	if (sp > 0)
-		push(ptop());
-	else
-		printf("error: stack empty\n");
-}
-
-/* swap: swap the top two elements */
-void swap(void)
-{
-	double op1, op2;
-
-	if (sp > 1) {
-		op2 = pop();
-		op1 = pop();
-		push(op2);
-		push(op1);
-	}
-	else
-		printf("error: stack too low to swap\n");
-}
-
 /* clear: clear the stack */
 void clear(void)
 {
 	sp = 0;
-	val[sp] = 0.0;
 }
 
-#include <ctype.h>
+unsigned long uf
+
+void math(char s[])
+{
+	/* to make use of the existing commands in math.h in a more natural sense,
+	we need to use string comparison instead to assigning characters to each command in math.h which
+	the total number of commands are not necessarily capped at the number of characters and symbols available to us.
+	we could use strcmp(), but that's technically not been introduced in the book yet.
+	this is against our approach (feels "cheating").
+	since it's also equally tedious to hard-code character comparions one by one,
+	let's have some fun coding with number */
+
+	int l = strlen(s);
+	int prime[] = {2, 3, 5, 7, 11, 13};
+	int i;
+	unsigned long n = 1;
+
+	for (i = 0; s[i] != '\0'; i++)
+		n = pow(n, s[i]);
+}
+
+
 
 int getch(void);
 void ungetch(int);
@@ -135,31 +156,39 @@ int getop(char s[])
 {
 	int i, c, sign;
 
-	while((s[0] = c = getch()) == ' ' || c == '\t')
+	/* read the fisrt non-space character */
+	while ((s[0] = c = getch()) == ' ' || c == '\t')
 		;
 	s[1] = '\0';
-	printf("initial s: %s\n", s);
 	i = 0;
-	if (c != '-' && !isdigit(c) && c != '.') {
-		/* collect string operator */
-		while ('A' <= (s[++i] = c = getch()) && c <= 'z')
-			;
-		s[i] = '\0';
-		return c;	/* not a number */
+	/* should understand the following commands:
+	 * topp, topd, tops, clear */
+	if (!isdigit(c) && c != '.') {
+		if (c == '-') { /* handle the negative sign separately */
+			if (isdigit(c = getch()) || c == '.')
+				s[++i] = c; /* negative number */
+			else {
+				ungetch(c);
+				return '-';
+			}
+		}
+		else {
+			return c;	/* not a number */
+		}
 	}
-	if (c == '-')	/* get ready to collect digits */
-		s[++i] = c = getch();
 	if (isdigit(c))	/* collect integer part */
 		while (isdigit(s[++i] = c = getch()))
-			printf("integer part: %d\n", c - '0');
+			;
 	if (c == '.')	/* collect fraction part */
 		while (isdigit(s[++i] = c = getch()))
-			printf("fraction part: %d\n", c - '0');
+			;
 	s[i] = '\0';
-	printf("terminal s: %s\n", s);
 	if (c != EOF) {
+		/* sending the remaining line to the buffer.
+		 * for example, if the input line was
+		 * "1 2 +\nEOF",
+		 * then in the first loop of reading, it should just read "1" and send the remaining " " to the buffer. */
 		ungetch(c);
-		printf("c != EOF, c: %d\n", c);
 	}
 	return NUMBER;
 }
