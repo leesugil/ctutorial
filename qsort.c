2919b22d1aa82eb7afb3a12022caf87f7717c044
leesugil <sugilmath@gmail.com>
2023-11-27 11:07:05 -0500

    quicksort

---
+++ qsort.c
@@ -0,0 +1,26 @@
+/* qsort: sort v[left], ..., v[right] into increasing order */
+void qsort(int v[], int left, int right)
+{
+	int i, last;
+	void swap(int v[], int i, int j);
+	if (left >= right)
+		return;
+	swap(v, left, (left + right)/2);
+	last = left;
+	for (i = left + 1; i <= right; i++)
+		if (v[i] < v[left])
+			swap(v, ++last, i);
+	swap(v, left, last);
+	qsort(v, left, last-1);
+	qsort(v, last+1, right);
+}
+
+/* swap: interchange v[i] and v[j] */
+void swap(int v[], int i, int j)
+{
+	int temp;
+
+	temp = v[i];
+	v[i] = v[j];
+	v[j] = temp;
+}
