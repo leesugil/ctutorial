bcdfbfcb0ebf8d27dbc4262d690db1826c63977a
leesugil <sugilmath@gmail.com>
2023-12-22 20:25:40 -0500

    exercise 5-16 almost finished - the concept is delivered, though it is not 100% working. For example, even if I use -fd, both individual examples for -f and -d seem to work fine, but an example combining both features kind of shows a problem

--- exercise5-16.c
+++ exercise5-16.c
@@ -13,12 +13,15 @@
 #define DIREC	8	/* 1000 directory order */
 #define LINES	100	/* max # of lines to be sorted */
 #define runqsort(x) (qsort2((void **) lineptr, 0, nlines-1, (int (*)(void *, void *)) x))	/* macro for qsort2 algorithm */
+#define runqsort3(x) (qsort3((void **) lineptr, 0, nlines-1, (int (*)(void *, void *, int)) x, option))	/* macro for qsort3 algorithm */
 
 int charcmp(char *, char *);
 int numcmp(char *, char *);
 int dircmp(char *, char *);
+int strcmp2(char *, char *, int);
 int readlines(char *lineptr[], int maxlines);
 void qsort2(void *v[], int left, int right, int (*comp)(void *, void *));
+void qsort3(void *v[], int left, int right, int (*comp)(void *, void *, int), int);
 void writelines(char *lineptr[], int nlines, int order);
 
 static char option = 0;
@@ -58,9 +61,9 @@ int main(int argc, char *argv[])
 			if (option & NUMERIC)
 				runqsort(numcmp);
 			else if (option & FOLD)
-				runqsort(charcmp);
+				runqsort3(strcmp2);
 			else
-				runqsort(strcmp);
+				runqsort3(strcmp2);
 			writelines(lineptr, nlines, option & DECR);
 		} else {
 			printf("input too big to sort\n");
@@ -91,6 +94,26 @@ void qsort2(void *v[], int left, int right, int (*comp)(void *, void *))
 	qsort2(v, last+1, right, comp);
 }
 
+/* qsort3: sort v[left]...v[right] into increasing order */
+void qsort3(void *v[], int left, int right, int (*comp)(void *, void *, int), int option)
+{
+	int i, last, mid;
+
+	void swap(void *v[], int, int);
+
+	if (left >= right)
+		return;
+	mid = (left + right) / 2;
+	if ((*comp)(v[left], v[right], option) > 0)
+		swap(v, left, mid);
+	last = left;
+	for (i = left+1; i <= right; i++)
+		if ((*comp)(v[i], v[left], option) < 0)
+			swap(v, ++last, i);
+	swap(v, left, last);
+	qsort3(v, left, last-1, comp, option);
+	qsort3(v, last+1, right, comp, option);
+}
 
 #define MAXLEN 1000
 /* readlines: read input lines */
@@ -162,11 +185,20 @@ int charcmp(char *s, char *t)
  * returns <0 if s<t, 0 if s==t, >0 if s>t */
 int dircmp(char *s, char *t)
 {
-	return *s - *t;
+	/* deprecated */
+	return 0;
 }
 
 /* strcmp2: returns <0 if s<t, 0 if s==t, >0 if s>t
  * accepts option for -fd */
 int strcmp2(char *s, char *t, int option)
 {
+	char a, b;	/* characters to compare depending on the option */
+	int f = option & FOLD;	/* 1 if fold */
+	int d = option & DIREC;	/* 1 if directory order */
+
+	for ((a = d ? ((isalnum(*s) || *s == ' ' || *s == '\0') ? (*s) : (' ')) : (*s)), b = (d ? ((isalnum(*t) || *t == ' ' || *t == '\0') ? (*t) : (' ')) : (*t)); f ? (tolower(a) == tolower(b)) : (a == b); s++, t++, a = d ? ((isalnum(*s) || *s == ' ' || *s == '\0') ? (*s) : (' ')) : (*s), b = d ? ((isalnum(*t) || *t == ' ' || *t == '\0') ? (*t) : (' ')) : (*t))
+		if (*s == '\0')
+			return 0;
+	return (f) ? (tolower(*s) - tolower(*t)) : (*s - *t);
 }
