67a0f0a5a3b83fad11f9f46997a57499f9eea5f3
Sugil Steve Lee <sugilmath@gmail.com>
2023-12-29 02:18:34 -0500

    a blueprint for more structured solution. - code not implemented yet. */

--- exercise5-17.c
+++ exercise5-17.c
@@ -1,5 +1,27 @@
 /* Add a field-searching capability, so sorting may be done on fields within lines, each field sorted according to an independant set of options. (The index for this book was sorted with -df for the index category and -n for the page numbers.) */
 
+/* Example: csv
+ *
+ * Input:
+ * %B,3
+ * *b,3
+ * &a,4
+ * *A,2
+ * @C,3
+ * #c,4
+ *
+ * Command: -df for index category, -n for page numbers
+ *
+ * Output:
+ * *A,2
+ * &a,4
+ * *b,3
+ * %B,3
+ * @C,3
+ * #c,4
+ * */
+
+
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
@@ -12,19 +34,18 @@
 #define FOLD	4	/* 0100 fold upper and lower cases */
 #define DIREC	8	/* 1000 directory order */
 #define LINES	100	/* max # of lines to be sorted */
-#define runqsort(x) (qsort2((void **) lineptr, 0, nlines-1, (int (*)(void *, void *)) x))	/* macro for qsort2 algorithm */
-#define runqsort3(x) (qsort3((void **) lineptr, 0, nlines-1, (int (*)(void *, void *, int)) x, option))	/* macro for qsort3 algorithm */
+#define runqsort(x, p, y, z, q) (qsort2((void **) p, 0, nlines-1, (int (*)(void *, void *, int)) x, y, z, q))
 
-int charcmp(char *, char *);
+int foldcmp(char *, char *);
 int numcmp(char *, char *);
 int dircmp(char *, char *);
 int strcmp2(char *, char *, int);
 int readlines(char *lineptr[], int maxlines);
-void qsort2(void *v[], int left, int right, int (*comp)(void *, void *));
-void qsort3(void *v[], int left, int right, int (*comp)(void *, void *, int), int);
+void qsort2(void *v[], int left, int right, int (*comp)(void *, void *, int), int, int, void *w[]);
 void writelines(char *lineptr[], int nlines, int order);
 
-static char option = 0;
+static int option = 0;	/* now int instead of char to hold up to the number of bytes an integer variable can hold in the system */
+static int field = 0;
 
 /* sort input lines */
 int main(int argc, char *argv[])
@@ -33,20 +54,20 @@ int main(int argc, char *argv[])
 	int nlines;
 	int c, rc = 0;
 
-	while (--argc > 0 && (*++argv)[0] == '-')
+	while (--argc > 0 && (*++argv)[0] == '-' && ++field > 0)
 		while ((c = *++argv[0]))
 			switch (c) {
 				case 'd':	/* directory order */
-					option |= DIREC;
+					option |= (DIREC + (field - 1) * 8);
 					break;
 				case 'f':	/* fold upper and lower cases */
-					option |= FOLD;
+					option |= (FOLD + (field - 1) * 8);
 					break;
 				case 'n':	/* numeric sort */
-					option |= NUMERIC;
+					option |= (NUMERIC + (field - 1) * 8);
 					break;
 				case 'r':	/* sort in decreasing order */
-					option |= DECR;
+					option |= (DECR + (field - 1) * 8);
 					break;
 				default:
 					printf("sort: illegal option %c\n", c);
@@ -54,14 +75,29 @@ int main(int argc, char *argv[])
 					rc = -1;
 					break;
 			}
+	printf("accepted option(s): %x, %d field(s)\n", option, field);
 	if (argc)
 		printf("Usage: <codename> -fnrd \n");
 	else {
+		/* sort must be done multiple times, hence changes here */
+		/* since it's a proptype, we assume the field sorting is always done starting from the initial field to subsequent fields */
+		/* to each comparison function, we provide the field number to work on as well which to the corresponding changes should be made in the function later in this work. */
 		if ((nlines = readlines(lineptr, LINES)) > 0) {
-			if (option & NUMERIC)
-				runqsort(numcmp);
-			else
-				runqsort3(strcmp2);
+			while (field > 0) {
+				/* this code also should turn back to the original idea with option & FOLD and option & DIREC showing up clearly in here */
+				/* in order for this to work, especially making FOLD and DIREC interchangeably used, should i make a copy of the original lines that's allowed to be modified such as masking certain characters for comparisons without hurting the original?
+				 * also, even if i do that, in which order FLD and DIREC sort should appear in here, if it's ever appropriate with the if-elif structure? */
+				char *linestocompare[LINES];
+				if (option & (FOLD + (field - 1) * 8))
+					;	/* determine whether to use the original lineptr or the tolower(*) version of it */
+				if (option & (NUMERIC + (field - 1) * 8))
+					runqsort(numcmp, lineptr, option, field--, linestocompare);
+				else if (option & (DIREC + (field - 1) * 8))
+					runqsort(dircmp, lineptr, option, field--, linestocompare);
+				else
+					runqsort(strcmp2, lineptr, option, field--, linestocompare);
+			}
+			printf("\n\n(sort) Printing Output...\n\n");
 			writelines(lineptr, nlines, option & DECR);
 		} else {
 			printf("input too big to sort\n");
@@ -72,8 +108,9 @@ int main(int argc, char *argv[])
 }
 
 /* qsort2: sort v[left]...v[right] into increasing order */
-void qsort2(void *v[], int left, int right, int (*comp)(void *, void *))
+void qsort2(void *v[], int left, int right, int (*comp)(void *, void *, int), int option, int field, void *w[])
 {
+	/* compare lines in w, swap v as swapping w */
 	int i, last, mid;
 
 	void swap(void *v[], int, int);
@@ -81,36 +118,20 @@ void qsort2(void *v[], int left, int right, int (*comp)(void *, void *))
 	if (left >= right)
 		return;
 	mid = (left + right) / 2;
-	if ((*comp)(v[left], v[right]) > 0)
+	if ((*comp)(w[left], w[right], option) > 0) {
 		swap(v, left, mid);
+		swap(w, left, mid);
+	}
 	last = left;
 	for (i = left+1; i <= right; i++)
-		if ((*comp)(v[i], v[left]) < 0)
+		if ((*comp)(w[i], w[left], option) < 0) {
 			swap(v, ++last, i);
+			swap(w, last, i);
+		}
 	swap(v, left, last);
-	qsort2(v, left, last-1, comp);
-	qsort2(v, last+1, right, comp);
-}
-
-/* qsort3: sort v[left]...v[right] into increasing order */
-void qsort3(void *v[], int left, int right, int (*comp)(void *, void *, int), int option)
-{
-	int i, last, mid;
-
-	void swap(void *v[], int, int);
-
-	if (left >= right)
-		return;
-	mid = (left + right) / 2;
-	if ((*comp)(v[left], v[right], option) > 0)
-		swap(v, left, mid);
-	last = left;
-	for (i = left+1; i <= right; i++)
-		if ((*comp)(v[i], v[left], option) < 0)
-			swap(v, ++last, i);
-	swap(v, left, last);
-	qsort3(v, left, last-1, comp, option);
-	qsort3(v, last+1, right, comp, option);
+	swap(w, left, last);
+	qsort2(v, left, last-1, comp, option, w);
+	qsort2(v, last+1, right, comp, option, w);
 }
 
 #define MAXLEN 1000
@@ -169,8 +190,8 @@ void swap(void *v[], int i, int j)
 	v[j] = temp;
 }
 
-/* charcmp: return <0 if s<t, 0 if s==t, >0 if s>t */
-int charcmp(char *s, char *t)
+/* foldcmp: return <0 if s<t, 0 if s==t, >0 if s>t */
+int foldcmp(char *s, char *t)
 {
 	for ( ; tolower(*s) == tolower(*t); s++, t++)
 		if (*s == '\0')
