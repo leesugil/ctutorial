0d7eaeb3f5edc14788da5796a6588cd1c291b11d
Sugil Steve Lee <sugilmath@gmail.com>
2023-12-31 15:43:20 -0500

    exercise 6-1 - skipped

---
+++ exercise6-1.c
@@ -0,0 +1,57 @@
+/* Our version of getword does not properly handle underscores, string constants, comments, or preprocessor control lines. Write a better version. */
+
+/* NOTE: Since this exercise is another tedious-exception-implementing-task-based one, even though it has a value to reinforce the learning process, I would consider it more seriously when I feel needed. This is due to my limited time constraint which is also, if not the most, important in real life. */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* THE CODE BELOW IS
+ * RESERVED FOR
+ * PERFORMANCE MEASURE */
+
+struct timeval start_wall, end_wall;
+double elapsed_time;
+clock_t start_cpu, end_cpu;
+double cpu_time_used;
+
+void time_measure_start(void)
+{
+	/* wall-clock time */
+	extern struct timeval start_wall, end_wall;
+	extern double elapsed_time;
+
+	gettimeofday(&start_wall, NULL);
+
+	/* cpu time */
+	extern clock_t start_cpu, end_cpu;
+	extern double cpu_time_used;
+
+	start_cpu = clock();
+}
+
+void time_measure_end(void)
+{
+	/* collect cpu time */
+	end_cpu = clock();
+	cpu_time_used = ((double) (end_cpu - start_cpu)) / CLOCKS_PER_SEC;
+	printf("\n\n\t*** END OF CODE ***\n\tCPU time used: %f seconds\n", cpu_time_used);
+
+	/* collect wall-clock time */
+	/* collect wall-clock time */
+	gettimeofday(&end_wall, NULL);
+	elapsed_time = (end_wall.tv_sec - start_wall.tv_sec) + (end_wall.tv_usec - start_wall.tv_usec) / 1000000.0;
+	printf("\n\tElapsed wall-clock time: %f seconds\n", elapsed_time);
+}
+
