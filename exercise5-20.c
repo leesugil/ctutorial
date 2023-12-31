82ed84dbad0291d230df245a747779cf263c3108
Sugil Steve Lee <sugilmath@gmail.com>
2023-12-31 00:59:22 -0500

    exercise 5-20 - temporarillly skipped for the same reason

---
+++ exercise5-20.c
@@ -0,0 +1,3 @@
+/* Expand dcl to handle declarations with function argument types, qualifiers like const, and so on. */
+
+/* NOTE: This exercise is left as a future project as I have to prioritize other things as well given the schedule and don't see the need of understanding the automatically handling difficult pointer expressions. However, I assume that exerything is intended by the author in this kind of textbooks, and will be back to this exercise if I encounter more important concepts later in the book based on this section. */
