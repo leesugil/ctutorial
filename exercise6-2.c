de8481c439a717d8b4a638646f621db39082510e
Sugil Steve Lee <sugilmath@gmail.com>
2024-01-03 16:03:04 -0500

    interim update - is_in_string and is_in_comment implemeted. next update: detect variable names from none-string-none-comment words

---
+++ exercise6-2.c
@@ -0,0 +1,235 @@
+/* Write a program that reads a C program and prints in alphabetical order each group of variable names that are identical in the first 6 characters, but different somewhere thereafter. Don't count words within strings and comments. Make 6 a parameter that can be set from the command line. */
+
+/* Main approach: upgrade the existing code by providing a conditional partial-sum (a "group-by" sum) feature with filtering. To do that, add a group member to tnode called "var". To detect variable names only and ignoring strings and comments, add a function to classify them and let the outcome determine ".var". When producing an output, add a feature to partial-sum by the first n=6 characters, filtered by ".var == some-valid-value". */
+
+/* challenge: sorting might get messy with the above method. we'll have "{x, var}", "{x, non-var}" at the same time, and the tree would become no longer binary (or we'd have another tree solely for variable names). */
+
+/* update: instead of adding ".var", add ".var_count" to count variable names seperately. */
+
+/* C template
+ * Author: Sugil Steve Lee
+ * Last Updated: 2023-12-30 */
+
+#include <stdio.h>
+#include <sys/time.h>
+#include <time.h>
+
+void time_measure_start(void);
+void time_measure_end(void);
+
+
+struct tnode {				/* the tree node: */
+	char *word;				/* points to the text */
+	int count;				/* number of occurrences */
+	int var_count;			/* variable name occurences */
+	struct tnode *left;		/* left child */
+	struct tnode *right;	/* right child */
+};
+
+#include <ctype.h>
+#include <string.h>
+#include "getword.c"
+
+#define MAXWORD 100
+struct tnode *addtree(struct tnode *, char *);
+void treeprint(struct tnode *);
+int getword(char *, int);
+void update_is_c_var(char *);
+char is_c_var = 0;			/* 1 if a variable name */
+
+/* word frequency count */
+int main(int argc, char *argv[])
+{
+	time_measure_start();	/* code speed measurement */
+	/* actual code
+	 * start here */
+
+	struct tnode *root;
+	char word[MAXWORD];
+
+	root = NULL;
+	while (getword(word, MAXWORD) != EOF) {
+		update_is_c_var(word);
+		if (isalpha(word[0]))
+			root = addtree(root, word);
+	}
+	treeprint(root);
+
+
+
+
+
+
+
+
+	/* end of
+	 * actual code */
+	time_measure_end();	/* collect time */
+	return 0;
+}
+
+struct tnode *talloc(void);
+char *strdup2(char *);
+
+/* addtree: add a node with w, at or below p */
+struct tnode *addtree(struct tnode *p, char *w)
+{
+	int cond;
+
+	if (p == NULL) {		/* a new word has arrived */
+		p = talloc();		/* make a new node */
+		p->word = strdup2(w);
+		p->var_count = (is_c_var) ? 1 : 0;
+		p->count = (is_c_var) ? 0 : 1;
+		p->left = p->right = NULL;
+	} else if ((cond = strcmp(w, p->word)) == 0)
+		if (is_c_var)
+			p->var_count++;
+		else
+			p->count++;		/* repeated word */
+	else if (cond < 0)		/* less than into left subtree */
+		p->left = addtree(p->left, w);
+	else					/* greater than into right subtree */
+		p->right = addtree(p->right, w);
+	return p;
+}
+
+char *vtype[] = {
+	"char",
+	"int",
+	"double",
+	"long"
+};
+int length = sizeof(vtype) / sizeof(vtype[0]);
+
+int is_in_string(char *);
+int is_in_comment(char *);
+
+/* update_is_c_var: updates is_c_var and within_string_or_comment */
+void update_is_c_var(char *w)
+{
+	is_c_var = (!is_in_string(w)
+			&& !is_in_comment(w))
+		? 1 : 0;
+}
+
+/* is_in_string: detects " but avoids \" */
+int is_in_string(char *w)
+{
+	static char in_string = 0;
+	static char p_char = '\0';
+	if (in_string == 0 && w[0] == '\"')
+		in_string = 1;
+	else if (in_string == 1
+			&& p_char != '\\'
+			&& w[0] == '\"')
+		in_string = 0;
+	p_char = w[0];
+	return in_string;
+}
+
+/* is_in_comment: detects \/\* and \/\* */
+int is_in_comment(char *w)
+{
+	static char in_comment = 0;
+	static char p_char = '\0';
+	if (in_comment == 0
+			&& p_char == '/'
+			&& w[0] == '*')
+		in_comment = 1;
+	else if (in_comment == 1
+			&& p_char == '*'
+			&& w[0] == '/')
+		in_comment = 0;
+	p_char = w[0];
+	return in_comment;
+}
+
+/* treeprint: in-order print of tree p */
+void treeprint(struct tnode *p)
+{
+	static char header = 1;
+	if (header == 1) {
+		printf("word\tcount\tvar_count\n");
+		header = 0;
+	}
+	if (p != NULL) {
+		treeprint(p->left);
+		printf("%s\t%4d\t%4d\n", p->word, p->count, p->var_count);
+		treeprint(p->right);
+	}
+}
+
+#include <stdlib.h>
+
+/* talloc: make a tnode */
+struct tnode *talloc(void)
+{
+	return (struct tnode *) malloc(sizeof(struct tnode));
+}
+
+char *strdup2(char *s)	/* make a duplicate of s */
+{
+	char *p;
+
+	p = (char *) malloc(strlen(s)+1);	/* +1 for '\0' */
+	if (p != NULL)
+		strcpy(p, s);
+	return p;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* THE CODE BELOW IS
+ * RESERVED FOR
+ * PERFORMANCE MEASURE */
+
+struct timeval start_wall, end_wall;
+double elapsed_time;
+clock_t start_cpu, end_cpu;
+double cpu_time_used;
+
+void time_measure_start(void)
+{
+	/* wall-clock time */
+	extern struct timeval start_wall, end_wall;
+	extern double elapsed_time;
+
+	gettimeofday(&start_wall, NULL);
+
+	/* cpu time */
+	extern clock_t start_cpu, end_cpu;
+	extern double cpu_time_used;
+
+	start_cpu = clock();
+}
+
+void time_measure_end(void)
+{
+	/* collect cpu time */
+	end_cpu = clock();
+	cpu_time_used = ((double) (end_cpu - start_cpu)) / CLOCKS_PER_SEC;
+	printf("\n\n\t*** END OF CODE ***\n\tCPU time used: %f seconds\n", cpu_time_used);
+
+	/* collect wall-clock time */
+	/* collect wall-clock time */
+	gettimeofday(&end_wall, NULL);
+	elapsed_time = (end_wall.tv_sec - start_wall.tv_sec) + (end_wall.tv_usec - start_wall.tv_usec) / 1000000.0;
+	printf("\n\tElapsed wall-clock time: %f seconds\n", elapsed_time);
+}
+
--- tree.c
+++ tree.c
@@ -9,22 +9,12 @@
 void time_measure_start(void);
 void time_measure_end(void);
 
-struct key {
-	char *word;
-	int count;
-} keytab[] = {
-	"auto", 0,
-	"break", 0,
-	"case", 0,
-	"char", 0,
-	"const", 0,
-	"continue", 0,
-	"default", 0,
-	/* ... */
-	"unsigned", 0,
-	"void", 0,
-	"volatile", 0,
-	"while", 0
+
+struct tnode {				/* the tree node: */
+	char *word;				/* points to the text */
+	int count;				/* number of occurrences */
+	struct tnode *left;		/* left child */
+	struct tnode *right;	/* right child */
 };
 
 #include <ctype.h>
@@ -32,30 +22,31 @@ struct key {
 #include "ungetch.c"
 
 #define MAXWORD 100
-#define NKEYS (sizeof keytab / sizeof(keytab[0]))
-
+struct tnode *addtree(struct tnode *, char *);
+void treeprint(struct tnode *);
 int getword(char *, int);
-struct key *
-binsearch(char *, struct key *, int);
 
-/* count C keywords - pointer version */
+/* word frequency count */
 int main(int argc, char *argv[])
 {
 	time_measure_start();	/* code speed measurement */
 	/* actual code
 	 * start here */
 
-
+	struct tnode *root;
 	char word[MAXWORD];
-	struct key *p;
 
+	root = NULL;
 	while (getword(word, MAXWORD) != EOF)
 		if (isalpha(word[0]))
-			if ((p=binsearch(word, keytab, NKEYS)) != NULL)
-				p->count++;
-	for (p = keytab; p < keytab + NKEYS; p++)
-		if (p->count > 0)
-			printf("%4d %s\n", p->count, p->word);
+			root = addtree(root, word);
+	treeprint(root);
+
+
+
+
+
+
 
 
 	/* end of
@@ -64,24 +55,54 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
-/* binsearch: find word in tab[0]...tab[n-1] */
-struct key *binsearch(char *word, struct key *tab, int n)
+struct tnode *talloc(void);
+char *strdup2(char *);
+
+/* addtree: add a node with w, at or below p */
+struct tnode *addtree(struct tnode *p, char *w)
 {
 	int cond;
-	struct key *low = &tab[0];
-	struct key *hight = &tab[n];
-	struct key *mid;
 
-	while (low < high) {
-		mid = low + (high-low) / 2;
-		if ((cond = strcmp(word, mid->word)) < 0)
-			high = mid;
-		else if (cond > 0)
-			low = mid + 1;
-		else
-			return mid;
+	if (p == NULL) {		/* a new word has arrived */
+		p = talloc();		/* make a new node */
+		p->word = strdup2(w);
+		p->count = 1;
+		p->left = p->right = NULL;
+	} else if ((cond = strcmp(w, p->word)) == 0)
+		p->count++;			/* repeated word */
+	else if (cond < 0)		/* less than into left subtree */
+		p->left = addtree(p->left, w);
+	else					/* greater than into right subtree */
+		p->right = addtree(p->right, w);
+	return p;
+}
+
+/* treeprint: in-order print of tree p */
+void treeprint(struct tnode *p)
+{
+	if (p != NULL) {
+		treeprint(p->left);
+		printf("%4d %s\n", p->count, p->word);
+		treeprint(p->right);
 	}
-	return NULL;
+}
+
+#include <stdlib.h>
+
+/* talloc: make a tnode */
+struct tnode *talloc(void)
+{
+	return (struct tnode *) malloc(sizeof(struct tnode));
+}
+
+char *strdup2(char *s)	/* make a duplicate of s */
+{
+	char *p;
+
+	p = (char *) malloc(strlen(s)+1);	/* +1 for '\0' */
+	if (p != NULL)
+		strcpy(p, s);
+	return p;
 }
 
 /* getword: get next word or character from input */
