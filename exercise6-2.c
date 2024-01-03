3e3700233fa3acccdbe8022a5dba04f596926b18
Sugil Steve Lee <sugilmath@gmail.com>
2024-01-03 18:03:02 -0500

    we can still provide weird inputs like int abc /* comment ; */ efg;, but will stop digging it here. next object: partial sum

--- exercise6-2.c
+++ exercise6-2.c
@@ -36,6 +36,8 @@ void treeprint(struct tnode *);
 int getword(char *, int);
 void update_is_c_var(char *);
 char is_c_var = 0;			/* 1 if a variable name */
+char in_comment = 0;		/* 1 if in comment */
+char in_string = 0;			/* 1 if in strinf */
 
 /* word frequency count */
 int main(int argc, char *argv[])
@@ -113,32 +115,32 @@ void update_is_c_var(char *w)
 	/* declaration keyword and ;*/
 	static int declaration = 0;		/* 1 if in declaration mode */
 	int i;
-	if (declaration == 0) {
-		for (i = 0; i < length; i++)
-			if (strcmp(w, vtype[i]) == 0)
-				declaration = 1;
+	if (!is_in_string(w) && !is_in_comment(w)) {
+		if (declaration == 0) {
+			for (i = 0; i < length; i++)
+				if (strcmp(w, vtype[i]) == 0)
+					declaration = 1;
+		}
+		else if (w[0] == ';')
+			declaration = 0;
 	}
-	else if (w[0] == ';')
-		declaration = 0;
 	/* strings, comments */
-	is_c_var = (declaration &&
-			(!is_in_string(w)
-			&& !is_in_comment(w)))
-		? 1 : 0;
+	is_c_var = (declaration) ? 1 : 0;
 }
 
 /* is_in_string: detects " but avoids \" */
 int is_in_string(char *w)
 {
 	/* should be ignored when in comment */
-	static char in_string = 0;
 	static char p_char = '\0';
-	if (in_string == 0 && w[0] == '\"')
-		in_string = 1;
-	else if (in_string == 1
-			&& p_char != '\\'
-			&& w[0] == '\"')
-		in_string = 0;
+	if (!in_comment) {
+		if (in_string == 0 && w[0] == '\"')
+			in_string = 1;
+		else if (in_string == 1
+				&& p_char != '\\'
+				&& w[0] == '\"')
+			in_string = 0;
+	}
 	p_char = w[0];
 	return in_string;
 }
@@ -147,19 +149,22 @@ int is_in_string(char *w)
 int is_in_comment(char *w)
 {
 	/* should be ignored when in string */
-	static char in_comment = 0;
 	static char p_char = '\0';
-	if (in_comment == 0
-			&& p_char == '/'
-			&& w[0] == '*')
-		in_comment = 1;
-	else if (in_comment == 1
-			&& p_char == '*'
-			&& w[0] == '/')
-		in_comment = 0;
+	if(!in_string) {
+		if (in_comment == 0
+				&& p_char == '/'
+				&& w[0] == '*')
+			in_comment = 1;
+		else if (in_comment == 1
+				&& p_char == '*'
+				&& w[0] == '/')
+			in_comment = 0;
+	}
 	p_char = w[0];
 	return in_comment;
 }
+
+void process_node(struct tnode *);
 
 /* treeprint: in-order print of tree p */
 void treeprint(struct tnode *p)
@@ -171,9 +176,14 @@ void treeprint(struct tnode *p)
 	}
 	if (p != NULL) {
 		treeprint(p->left);
-		printf("%s\t%4d\t%4d\n", p->word, p->count, p->var_count);
+		process_node(p);
 		treeprint(p->right);
 	}
+}
+
+void process_node(struct tnode *p)
+{
+	printf("%s\t%4d\t%4d\n", p->word, p->count, p->var_count);
 }
 
 #include <stdlib.h>
