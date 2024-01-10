c52d7f15f25d02031ec9464c4232142b83c32cda
leesugil <sugilmath@gmail.com>
2024-01-10 16:53:54 -0500

    exercise 6-6

---
+++ exercise6-6.c
@@ -0,0 +1,3 @@
+/* Implement a simple version of the #define processor (i.e., no arguments) suitable for use wth C programs, based on the routines of this section. You may also find getch and ungetch helpful. */
+
+/* note: it's not clear what the problem is asking for. we'll get back to this once this exercise turns out to be more significant in later exercise problems. otherwise i feel okay learning about the basic table lookup routine in this section. moving on. */
