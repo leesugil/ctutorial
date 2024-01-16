74ec4641c19ea7e2fcb4803adb421ba9602c9d64
leesugil <sugilmath@gmail.com>
2024-01-15 21:23:44 -0500

    opens file given file names

--- exercise7-7.c
+++ exercise7-7.c
@@ -2,12 +2,14 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define MAXLINE 1000
 
 struct {
 	unsigned int except : 1;
 	unsigned int number : 1;
+	unsigned int file : 1;
 } flags;
 
 /* find: print lines that match pattern from 1st arg from files*/
@@ -17,22 +19,40 @@ int main(int argc, char *argv[])
 	size_t mline = 0;
 	long lineno = 0;
 	int c, found;
+	FILE *fp;
+	char *prog = argv[0];
 
-	while (--argc > 0 && (*++argv)[0] == '-')
-		while ((c = *++argv[0]))
-			switch (c) {
-				case 'x':
-					flags.except = 1;
-					break;
-				case 'n':
-					flags.number = 1;
-					break;
-				default:
-					printf("find: illegal option %c\n", c);
-					argc = 0;
-					found = -1;
-					break;
+	while (--argc > 0) {
+		if ((*++argv)[0] == '-' && flags.file != 1) {
+			while ((c = *++argv[0]))
+				switch (c) {
+					case 'x':
+						flags.except = 1;
+						break;
+					case 'n':
+						flags.number = 1;
+						break;
+					case 'f':
+						flags.file = 1;
+						break;
+					default:
+						printf("find: illegal option %c\n", c);
+						argc = 0;
+						found = -1;
+						break;
+				}
+		}
+		else if (flags.file == 1) {
+			/* open file and feed */
+			if ((fp = fopen(*argv, "r")) == NULL) {
+				fprintf(stderr, "%s: can't open %s\n", prog, *argv);
+				exit(1);
+			} else {
+				/* feed */
+				fclose(fp);
 			}
+		}
+	}
 	
 	if (argc != 1)
 		printf("Usage: find -x -n pattern -f filename1 filename2 ...\n");
