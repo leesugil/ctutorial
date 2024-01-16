3b30045f32c05180236527e421d969e0b7f0e8af
leesugil <sugilmath@gmail.com>
2024-01-16 13:05:02 -0500

    exercise 7-9

---
+++ exercise7-9.c
@@ -0,0 +1,4 @@
+/* Functions like isupper can be implemented to save space or time. Explore both possibilities. */
+
+/* discussion: what would be the benefit of storing characters all in the upper case. would it save some storage space used for the system storing ASCII data for lower cases? i can see how putting everything in upper case saving time: all kind of character comparisons would be made a few lines of codes simpler. */
+
