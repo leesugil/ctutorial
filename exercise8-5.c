58018836870316c91bbdc91a60de1a276d4bdfd8
leesugil <sugilmath@gmail.com>
2024-01-17 11:35:22 -0500

    exercise 8-5 - skipped, see file for comment

---
+++ exercise8-5.c
@@ -0,0 +1,4 @@
+/* Modify the fsize program to print the other information contained in the node entry. */
+
+/* i find examples shown in this chapter too system-dependant and possibly outdated for modern devices. studied a little to look into stdio.h and _stdio.h in my macbook to look up valid variable names for, for example, _READ, _WRITE, _UNBUF, OPEN_MAX, ctr, ptr, etc., and i find it too time-consuming to fix everything to make the example work on my macbook even though it is tempting to master this part for system engineering. maybe i'll need to come back to this part later in my life, and maybe today is the best day to learn about the subject, but even if i can't follow everything in this chapter later in the future, it'll not be a total waste of time but a chance to reinforce what i learned. */
+
